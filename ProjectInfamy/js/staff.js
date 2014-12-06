ed88c5a4b1f218a6eebb243ffafef52b6150c07e
VonMises <jason.hughes@outlook.com>
2014-12-06 07:10:27 -0600

    refactor

---
+++ ProjectInfamy/js/staff.js
@@ -0,0 +1,21 @@
+function draw_canvas() {
+    var canvas = document.getElementById("canvas1");
+    var context = canvas.getContext("2d");
+
+    for (var y = 50; y < 175; y += 25) {
+        context.moveTo(100, y);
+        context.lineTo(600, y);
+    }
+
+    context.stroke();
+
+    context.font = "bold 28px sans-serif";
+    context.fillText("\u266a", 250, 50);
+    context.fillText("\u266b", 300, 150);
+}
+
+function clear_canvas() {
+    var canvas = document.getElementById("canvas1");
+    canvas.width = canvas.width;
+}
+
