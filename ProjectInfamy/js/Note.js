28ffa36bd226a87855037f0160240b40aba72689
Auttcast <b.mccartney1@gmail.com>
2014-12-07 20:25:35 -0600

    basic vex flow (lacking measure/line)

--- ProjectInfamy/js/Note.js
+++ ProjectInfamy/js/Note.js
@@ -24,6 +24,21 @@ ProcessNoteQueue = function()
 
 }
 
+GetVexFormat = function(midiNoteType, noteTiming)
+{
+    var result = ':' + noteTiming + ' ';
+
+    if(midiNoteType.length == 3)
+    {
+        result += midiNoteType[0] + '@' + '/' + midiNoteType[2];
+    } else {
+
+        result += midiNoteType[0] + '/' + midiNoteType[1];
+    }
+
+    return result;
+}
+
 CreateNote = function(noteKey, duration)
 {
     return {
@@ -43,27 +58,38 @@ CreateRest = function(duration)
 
 var wholeNoteLength = 1100;
 
+SixteenthNote = function(data)
+{
+    var note = CreateNote(data, wholeNoteLength / 16);
+    note.vexFormat = GetVexFormat(data, '16');
+    NoteQueue.unshift(note);
+}
+
 EighthNote = function(data)
 {
     var note = CreateNote(data, wholeNoteLength / 8);
+    note.vexFormat = GetVexFormat(data, '8');
     NoteQueue.unshift(note);
 }
 
 QuarterNote = function(data)
 {
     var note = CreateNote(data, wholeNoteLength / 4);
+    note.vexFormat = GetVexFormat(data, 'q');
     NoteQueue.unshift(note);
 }
 
 HalfNote = function(data)
 {
     var note = CreateNote(data, wholeNoteLength / 2);
+    note.vexFormat = GetVexFormat(data, 'h');
     NoteQueue.unshift(note);
 }
 
 WholeNote = function(data)
 {
     var note = CreateNote(data, wholeNoteLength);
+    note.vexFormat = GetVexFormat(data, 'w');
     NoteQueue.unshift(note);
 }
 
@@ -71,26 +97,37 @@ WholeNote = function(data)
 
 
 
+SixteenthRest = function()
+{
+    var note = CreateRest(data, wholeNoteLength / 16);
+    note.vexFormat = GetVexFormat(data, '16');
+    NoteQueue.unshift(note);
+}
+
 EighthRest = function()
 {
     var note = CreateRest(data, wholeNoteLength / 8);
+    note.vexFormat = GetVexFormat(data, '8');
     NoteQueue.unshift(note);
 }
 
 QuarterRest = function()
 {
     var note = CreateRest(data, wholeNoteLength / 4);
+    note.vexFormat = GetVexFormat(data, 'q');
     NoteQueue.unshift(note);
 }
 
 HalfRest = function()
 {
     var note = CreateRest(data, wholeNoteLength / 2);
+    note.vexFormat = GetVexFormat(data, 'h');
     NoteQueue.unshift(note);
 }
 
 WholeRest = function()
 {
     var note = CreateRest(data, wholeNoteLength);
+    note.vexFormat = GetVexFormat(data, 'w');
     NoteQueue.unshift(note);
 }
